cb6f5e06a65a9d3172adac1627a908b4fcb85aec
MariaFernanda1818 <83258884+MariaFernanda1818@users.noreply.github.com>
2024-10-06 19:05:39 -0500

    Se crea entity dto y mapper de tabla nueva

---
+++ src/main/java/com/sistema/sah/commons/entity/BlackListTokenEntity.java
@@ -0,0 +1,31 @@
+package com.sistema.sah.commons.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "black_list_token")
+public class BlackListTokenEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "black_list_token_id_gen")
+    @SequenceGenerator(name = "black_list_token_id_gen", sequenceName = "black_list_token_id_gen", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "token", nullable = false, length = 512)
+    private String token;
+
+    @Column(name = "fecha_expiracion", nullable = false)
+    private LocalDateTime fechaExpiracion;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "codigo_usuario_fk", nullable = false)
+    private UsuarioEntity codigoUsuarioEntityFk;
+
+
+}
